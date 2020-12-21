f6fa0045fa3159ed86a0f15a06abfe9e3d7ba6e9
Jaewon Kim <jaewon8668@gmail.com>
2020-12-21 19:13:21 +0900

    JS:toggleBtn and CSS navbar

--- main.js
+++ main.js
@@ -14,3 +14,13 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+//Make menuBar when screen is going to small
+const toggleBtn = document.querySelector('.navbar__toggleBtn');
+const menu = document.querySelector('.navbar__menu');
+const icons = document.querySelector('.navbar__icons');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+});
